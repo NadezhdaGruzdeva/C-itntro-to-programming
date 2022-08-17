217a5c0513962e1f069ad82571cc344bb882f894
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-17 21:54:07 +0300

    Updated 007

--- C# Lesson 1 ex 7 cercles/Program.cs
+++ C# Lesson 1 ex 7 cercles/Program.cs
@@ -4,7 +4,7 @@
 
 int xa = 40, ya = 1,
     xb = 1, yb = 30,
-    xc = 80, yc = 30;
+    xc = 80, yc = 30; //определили 3 вершины треугольника
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");    
@@ -13,20 +13,20 @@ Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");   
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");   
+Console.WriteLine("+");   //вершины
 
 
-int x = xa, y = xb;
+int x = xa, y = xb;  //выбираем 2 любых точки
 
 int count = 0;
 
 while(count < 1000)
 {
-    int what = new Random().Next(0,3); // [0;3) 0 1 2
+    int what = new Random().Next(0,3); // [0;3) 0 1 2 генерация случайных чисел (выбираем вершуну нужного нам треугольника)
     if (what == 0)    
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x + xa) / 2; 
+        y = (y + ya) / 2;//находим середины между 2 точками
     }
 
     if (what == 1)
@@ -43,5 +43,5 @@ while(count < 1000)
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count = count + 1;
+    count = count + 1; //count++ or count =+1
 }
