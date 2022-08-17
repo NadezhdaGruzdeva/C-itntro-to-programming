2f991b6dc62f45b81c3d10f977a6eee76fa3f82a
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-17 14:39:52 +0300

    --5 ELSE IF

---
+++ C# Lesson 1 ex 5 ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Input username");
+string username = Console.ReadLine();
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine ("Ooo yeah, you are Maria");
+}
+else
+{
+    Console.Write ("Hi ");
+    Console.WriteLine(username);
+}
