d8ef6d82694044b57c803510fa876e455600c16b
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-18 21:06:58 +0300

    Semina 1 ex 2

---
+++ C# Seminar 1 ex 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number1 ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number2 ");
+int number2 = int.Parse(Console.ReadLine());
+ 
+int sqrNumer2 = number2 * number2;
+if (sqrNumer2 == number1)
+{
+    Console.Write("Sqr of number 2 is equal number 1");
+}
+else
+{
+    Console.Write("Sqr of number 2 is unequal number 1");   
+}
+
+
+
+//Console.Write(sqrNumer1);
+
+
+
