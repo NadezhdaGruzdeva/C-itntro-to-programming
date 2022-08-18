b8e1cd566153430794ba598fa3c62034a80a2ca3
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-18 21:49:41 +0300

    Sem 1 ex 4)

---
+++ C# Seminar 1 ex 4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+int devidedNumber = number / 10;
+int ostatok = number - devidedNumber *10;
+
+Console.WriteLine(ostatok);
