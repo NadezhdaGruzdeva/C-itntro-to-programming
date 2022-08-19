5ba239637f256cf630c5ca886ee28ba6f5ac705d
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-19 12:05:57 +0300

    C# HW 1 Task 4

---
+++ C# HW 1/C# HW 1 Task 4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+//data unput
+Console.Write("Enter the first  number ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Enter the third  number ");
+int number3 = int.Parse(Console.ReadLine());
+
+//If-Else 
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.Write("Maximum number is ");
+        Console.Write(number1);
+    }
+    else
+    {
+        Console.Write("Maximum number is ");
+        Console.Write(number3);
+    }
+}
+else
+{
+    if (number2 > number3)
+    {
+        Console.Write("Maximum number is ");
+        Console.Write(number2);
+    }
+    else
+    {
+        Console.Write("Maximum number is ");
+        Console.Write(number3);
+    }
+}
