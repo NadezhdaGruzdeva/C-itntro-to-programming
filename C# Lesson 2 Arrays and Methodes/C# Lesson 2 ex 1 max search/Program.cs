310cd5ad99d10f9da08b5366cdbc1303dfdb8557
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-21 09:54:17 +0300

    L2 ex 1

---
+++ C# Lesson 2 Arrays and Methodes/C# Lesson 2 ex 1 max search/Program.cs
@@ -0,0 +1,27 @@
+﻿// Поиск максимального из 9 чисел
+int a1 = 1;
+int b1 = 2;
+int c1 = 6;
+int a2 = 1;
+int b2 = 2;
+int c2 = 6;
+int a3 = 310;
+int b3 = 2;
+int c3 = 6;
+
+int max =a1;
+
+if (b1>max) max = b1;
+if (c1>max) max = c1;
+
+if (a2>max) max = a2;
+if (b2>max) max = b2;
+if (c2>max) max = c2;
+
+if (a3>max) max = a3;
+if (b3>max) max = b3;
+if (c3>max) max = c3;
+
+
+
+Console.WriteLine(max);
