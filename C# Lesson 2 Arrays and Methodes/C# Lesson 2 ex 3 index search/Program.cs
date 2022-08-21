a4170992208386fc946023987caa30ccf755e9cf
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-08-21 10:52:28 +0300

    l2 ex 3

---
+++ C# Lesson 2 Arrays and Methodes/C# Lesson 2 ex 3 index search/Program.cs
@@ -0,0 +1,21 @@
+﻿// поиск индекса с нужным значением
+int [] array ={4,4,3,4,5,6,7,81,9};
+//количество эл массива
+int n = array.Length;
+
+//искомое значение
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        //прекратить работу программы
+        break;
+    }
+    //index = index +1;
+    index++;
+}
+
