c885eaf7335c7f37f0e2bececc0b99d72203ab2d
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-09-06 22:00:32 +0300

    L4

---
+++ C# Lesson 4  Recursia and Dvumernie Arrais/C# Lesson 4 ex 1 Output 2xArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Output 2x array
+
+// string[,] table = new string [2, 5];
+// table [1, 2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//  for (int columns = 0; columns < 5; columns++)
+//  {
+//     Console.WriteLine($"--{table[rows, columns]}");
+//  }
+
+// }
+
+int[,] matrix = new int[3,4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+ for (int j = 0; j < matrix.GetLength(1); j++)
+ {
+    Console.Write($"{matrix[i, j]} ");
+ }
+Console.WriteLine();
+}
---
+++ C# Lesson 4  Recursia and Dvumernie Arrais/C# Lesson 4 ex 5 Recursia fibanach/Program.cs
@@ -0,0 +1,18 @@
+﻿// Fibanachi
+//f(1)=1
+//f(2)=1
+//f(3)=2
+//f(4)=3
+//f(5)=5
+
+double Fibanachi (int n)
+{
+    if(n == 1 || n == 2) return 1; 
+    else return Fibanachi(n-1) + Fibanachi(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i} - {Fibanachi(i)}");
+}
+    
