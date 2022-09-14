8a22558eaf26bb4571f8ec150e8f04ca9dc7b643
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-09-14 11:54:37 +0300

    ALL f

--- C# All Functions/Program.cs
+++ C# All Functions/Program.cs
@@ -212,6 +212,25 @@ int IndexOf(int[] colleccion, int find)
     return position;
 }
 
+//Поиск макимума в 2х массиве
+int MinValueSearch(int[,] array)
+{
+    int rowsQty = array.GetLength(0);
+    int columnsQty = array.GetLength(1);
+    int minValue = array[0,0];
+
+    for (int i = 0; i < rowsQty; i++)
+    {
+        for (int j = 0; j < columnsQty; j++)
+        {
+            if (array[i, j] < minValue)
+            {
+                minValue = array[i, j];
+            }
+        }
+    }
+return
+}
 //Create array of real numbers (maybe so long but IDK how to make it shoter)
 double[] GetArray (int sizeGetArray, int minValueGetArray, int maxValueGetArray)
 {
@@ -223,5 +242,176 @@ double[] GetArray (int sizeGetArray, int minValueGetArray, int maxValueGetArray)
         arrayGetArray[i] = intPartGetArray + fractPartGetArray;
     }
     return arrayGetArray;
+}
+
+//Сортировка от макс к мин построчно в двумерном массиве
+void SortMaxToMinByRows(int[,] array)
+{
+    int rowsQty = array.GetLength(0);
+    int columnsQty = array.GetLength(1);
+    for (int i = 0; i < rowsQty; i++)
+    {
+        for (int j = 0; j < columnsQty; j++)
+        {
+            for (int k = 0; k < columnsQty-1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temporary = array[i, k + 1];
+                    array[i,k + 1] = array[i, k];
+                    array[i, k] = temporary;
+                }
+            }
+        }
+    }
+}
+
+//Fill arrray with unique values
+int[] GetArrayWithUniqueValues(int rowQtyGetArray, int columnQtyGetArray, int deepQtyGetArray, int minValueGetArray, int maxValueGetArray)
+{
+    //int[,,] result = new int[rowQtyGetArray, columnQtyGetArray, deepQtyGetArray];
+    
+    int sizeFlatArray = rowQtyGetArray * columnQtyGetArray * deepQtyGetArray;
+    int[] flatResult = new int[sizeFlatArray]; //для последующего поиска дубликатов
+    bool isUnique = true;
+    flatResult[0] = new Random().Next(minValueGetArray, maxValueGetArray + 1);
+
+    int indexForFlatArray = 1;
+    while (indexForFlatArray < sizeFlatArray)//заполняем массив ранд знач
+    {           
+        flatResult[indexForFlatArray] = new Random().Next(minValueGetArray, maxValueGetArray + 1);
+        
+        for (int i = 0; i < indexForFlatArray; i++) //сравниваем с ранее введенными
+        {
+            if (flatResult[indexForFlatArray] == flatResult[i]) 
+            {
+                isUnique = false;
+                break;
+            }
+            else
+            {
+                isUnique = true;
+            }
+        } 
+        if (isUnique == false) continue;   
+        else indexForFlatArray++;
+    }
+return flatResult;
+}
+
+void Print3xArrayByRows(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int k = 0; k < matr.GetLength(2); k++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                Console.Write($"{matr[i, j, k]}({i},{j},{k}) \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+//Fill arrray 3x with unique values
+int[,,] Get3xArrayWithUniqueValues(int rowQtyGetArray, int columnQtyGetArray, int deepQtyGetArray, int minValueGetArray, int maxValueGetArray)
+{
+    int sizeFlatArray = rowQtyGetArray * columnQtyGetArray * deepQtyGetArray;
+    int[] flatResult = new int[sizeFlatArray]; //для последующего поиска дубликатов
+    bool isUnique = true;
+    flatResult[0] = new Random().Next(minValueGetArray, maxValueGetArray + 1);
+
+    int indexForFlatArray = 1;
+    while (indexForFlatArray < sizeFlatArray)//заполняем массив ранд знач
+    {           
+        flatResult[indexForFlatArray] = new Random().Next(minValueGetArray, maxValueGetArray + 1);
+        
+        for (int i = 0; i < indexForFlatArray; i++) //сравниваем с ранее введенными
+        {
+            if (flatResult[indexForFlatArray] == flatResult[i]) 
+            {
+                isUnique = false;
+                break;
+            }
+            else
+            {
+                isUnique = true;
+            }
+        } 
+        if (isUnique == false) continue;   
+        else indexForFlatArray++;
+    }
+Console.WriteLine("1x array");
+Console.WriteLine(String.Join(" ", flatResult));
+
+int [,,] result = new int[rowQtyGetArray, columnQtyGetArray, deepQtyGetArray];
+indexForFlatArray = 0;
+for (int i = 0; i < rowQtyGetArray; i++)
+    {
+    for (int j = 0; j < columnQtyGetArray; j++)
+        {
+            for (int k = 0; k < deepQtyGetArray; k++)
+            {
+                result[i, j, k] = flatResult[indexForFlatArray];
+                indexForFlatArray++;
+            }
+        }
+    }
+return result;
+
+}
+
+//круголя
+void RounWalkingThroughArray(int[,] RWTArray)
+{
+    int qtyRows = RWTArray.GetLength(0);
+    int qtyColumns = RWTArray.GetLength(1);
+
+    //оперделяем колво кругов как минимальная сторона массива + 1
+    //Эмперич методом выявл зак-ть для мин ст масиива нужны сл кол-во кругов соответственно
+    // 1-1, 2-1, 3-2, 4-2, 5-3, 6-3, 7 -4
+    int circles =0;
+    if (qtyRows < qtyColumns) circles = (qtyRows +1)/2;
+    else circles = (qtyColumns +1)/2;
+
+
+    for (int swit4 = 0; swit4 < circles; swit4++)
+    {
+        //1 круг = 4 отрезка
+        Console.Write($"{swit4 +1} cyrcle ");
+
+        int j =0; //чтобы не было ошибок при повт вызове в др частях
+
+        // 1 отрезок
+        int i = qtyRows-1 -swit4;
+
+        for (j = 0 + swit4;j <  qtyColumns - swit4;j++)
+        {
+            Console.Write($"{RWTArray[i,j]} ");
+        }
+
+        // 2 отрезок
+        j = qtyColumns-1 - swit4;
+        for (i = qtyRows-2 - swit4; i >= 0 + swit4;i--)
+        {
+            Console.Write($"{RWTArray[i,j]} ");
+        }
+
+        // 3 отрезок
+        i = 0 + swit4;
+        for (j = qtyColumns-2 - swit4; j >= 0 + swit4; j--)
+        {
+            Console.Write($"{RWTArray[i,j]} ");
+        }
+
+        // 4 отрезок 
+        j = 0 + swit4;
+        for (i = 1 + swit4; i <  qtyRows-1 -swit4; i++)
+        {
+            Console.Write($"{RWTArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
 }
 
