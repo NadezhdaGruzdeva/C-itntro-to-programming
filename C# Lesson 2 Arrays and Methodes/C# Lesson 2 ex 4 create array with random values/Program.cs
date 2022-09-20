e095526f709da1eac949a0e9712c84d3391e147f
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-09-20 11:23:40 +0300

    L2

---
+++ C# Lesson 2 Arrays and Methodes/C# Lesson 2 ex 4 create array with random values/Program.cs
@@ -0,0 +1,54 @@
+﻿
+//Заполнение массива рандомными значениями
+void FillArray (int [] colletion)
+{
+    int length = colletion.Length;
+    int index = 0;
+    while (index < length)
+    {
+        //присваивание рандомных значений
+        colletion[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+//print array
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+     Console.WriteLine(col[position]);
+     position ++;   
+    }
+}
+
+//Метод возвращающий первый индекс искомого значения
+int IndexOf(int[] colleccion, int find)
+{
+    int count = colleccion.Length;
+    int index = 0;
+
+    int position =-1; //-1-выходит когда элемент не найден
+
+
+    while (index<count)
+    {
+        if (colleccion[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FillArray (array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = IndexOf(array,4);
+Console.WriteLine (pos);
