00b8e29a983ce414f039af6fc8bfea399f039598
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-09-20 11:24:55 +0300

    S

---
+++ C# Seminar 5/C# Seminar 35 task/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив со 123 случайных чиселю
+//Найдите кол-во элементгов массива значения кот лежат в диапозоне [10, 99]
+
+// Определить присутствует ли заданное значение в массиве
+
+// генерация массива
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int LookingForValue(int[] arrayLook, int minSearchingValue, int maxSearchingValue)
+{
+    int qtySearchValues = 0;
+    for (int i = 0; i < arrayLook.Length; i++)
+    {
+        if (arrayLook[i] >= minSearchingValue & arrayLook[i] <= maxSearchingValue)
+         qtySearchValues ++;
+    }
+    return qtySearchValues;
+}
+
+int [] myArray = GetArray (123, 0, 2000);
+Console.WriteLine(String.Join(" ", myArray));
+
+int result = LookingForValue(myArray, 10, 99);
+Console.WriteLine(result);
+
---
+++ C# Seminar 5/C# Seminar 37 task/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний и тд
+
+
+// генерация массива
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int [] PairMultipl(int[] arrayPairMultipl)
+{
+    int sizepMultArray =  (arrayPairMultipl.Length + 1)/2;
+    int [] pMultArray = new int [sizepMultArray];
+    for (int i = 0; i < sizepMultArray; i++)
+    {
+        if (i == arrayPairMultipl.Length - 1 - i)
+        {
+            pMultArray [i] = arrayPairMultipl[i];
+        }  
+        else
+        {
+            pMultArray [i] = arrayPairMultipl[i] * arrayPairMultipl[arrayPairMultipl.Length - 1 - i];
+        }
+    }
+    return pMultArray;
+}
+
+int [] myArray = GetArray (6, 0, 6);
+Console.WriteLine(String.Join(" ", myArray));
+
+int [] result = PairMultipl(myArray);
+Console.WriteLine(String.Join(" ", result));
